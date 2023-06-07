b05a6ab9781e89c4daf1c9f229a0f3ac8703dfb6
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-07 23:26:25 +0800

    Setup MongoDB and create data models

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
 
 const port = process.env.PORT || 8000
 const app = express()
+
+connectDB() // Connect to MongoDB
 
 app.get('/', (req, res) => {
     res.send('API is running...')
