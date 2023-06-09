2db9c788454de1c777a83b1e83140bfd9e2b3468
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-10 00:08:26 +0800

    Import Sample Data to MongoDB

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -13,7 +13,7 @@ const orderSchema = mongoose.Schema({
             image: { type: String, required: true },
             price: { type: Number, required: true },
             product: {
-                type: mongoose.Schema.Types.ObjecId,
+                type: mongoose.Schema.Types.ObjectId,
                 required: true,
                 ref: 'Product'
             }
