842a4c7f6bb3337e908fdd4e61f48fd9b7fb8f23
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-13 04:06:09 +0800

    Add Custom Error Middleware and Product Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,9 @@
 import express from 'express'
-import products from '../data/products.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json(products)
-})
-
-router.get('/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -15,5 +16,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
