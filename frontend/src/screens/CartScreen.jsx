001555ccba6b63f34c2e9890b615d360fbeffc78
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-16 00:00:15 +0800

    Add Remove from Cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { FaTrash } from 'react-icons/fa'
 import Message from '../components/Message'
-import { addToCart } from '../slices/cartSlice'
+import { addToCart, removeFromCart } from '../slices/cartSlice'
 
 const CartScreen = () => {
     const navigate = useNavigate()
@@ -13,6 +13,12 @@ const CartScreen = () => {
     const { cartItems } = cart
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}))
+    }
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id))
+    }
+    const checkoutHandler = () => {
+        navigate('/login?redirect=/shipping')
     }
 
     return (
@@ -47,7 +53,7 @@ const CartScreen = () => {
                                         </Form.Control>
                                     </Col>
                                     <Col md={2}>
-                                        <Button type='button' variant='light'>
+                                        <Button type='button' variant='light' onClick={() => removeFromCartHandler(item._id)}>
                                             <FaTrash />
                                         </Button>
                                     </Col>
@@ -69,7 +75,7 @@ const CartScreen = () => {
                             </h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Button type='button' className='btn-block' disabled={cartItems.length === 0}>
+                            <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler()}>
                                 Proceed to Checkout
                             </Button>
                         </ListGroup.Item>
