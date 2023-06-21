e947a1f095628a799a38521fcb926c4c7f1595c1
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-21 22:17:52 +0800

    Add User Email & Password Validation

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ const app = express()
 
 connectDB() // Connect to MongoDB
 
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
