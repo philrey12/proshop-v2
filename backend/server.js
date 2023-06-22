efa80815b71e3ed4e5aa0dce4d08f3fce4a4c61e
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-22 18:48:33 +0800

    Add Auth/Login Middleware, Logout User & Clear Cookie, User Register, and User Profile Endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
@@ -15,6 +16,9 @@ connectDB() // Connect to MongoDB
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('API is running...')
