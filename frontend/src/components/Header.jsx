78c28c37f351f784ecb0ddd716fc0b02d0f93a60
Philip Rey Raquinel <devphil.work@gmail.com>
2023-06-27 11:14:29 +0800

    Add User Logout and Registration functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import { logout } from '../slices/authSlice'
 import logo from '../assets/logo.png'
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart)
     const { userInfo } = useSelector((state) => state.auth)
-    const logoutHandler = () => {
-        console.log('logout')
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [logoutApiCall] = useLogoutMutation()
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap()
+            dispatch(logout())
+            navigate('/login')
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     return (
@@ -51,7 +63,7 @@ const Header = () => {
                             ) : (
                                 <LinkContainer to='/login'>
                                     <Nav.Link href='/login'>
-                                        <FaUser /> Sign In
+                                        <FaUser /> Log In
                                     </Nav.Link>
                                 </LinkContainer>
                             ) }
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -5,28 +5,35 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import Loader from '../components/Loader'
-import { useLoginMutation } from '../slices/usersApiSlice'
+import { useRegisterMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/authSlice'
 import { toast } from 'react-toastify'
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const [login, { isLoading }] = useLoginMutation()
+    const [register, { isLoading }] = useRegisterMutation()
     const { userInfo } = useSelector((state) => state.auth)
     const { search } = useLocation()
     const sp = new URLSearchParams(search)
     const redirect = sp.get('redirect') || '/'
     const submitHandler = async (e) => {
         e.preventDefault()
-        try {
-            const res = await login({ email, password }).unwrap()
-            dispatch(setCredentials({ ...res }))
-            navigate(redirect)
-        } catch (err) {
-            toast.error(err?.data?.message || err.error)
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match')
+            return
+        } else {
+            try {
+                const res = await register({ name, email, password }).unwrap()
+                dispatch(setCredentials({ ...res }))
+                navigate(redirect)
+            } catch (err) {
+                toast.error(err?.data?.message || err.error)
+            }
         }
     }
 
@@ -41,8 +48,13 @@ const LoginScreen = () => {
             {/* <h2>Sign In</h2> */}
 
             <Form className='mt-4' onSubmit={submitHandler}>
+                <Form.Group controlId='name' className='my-3'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type='text' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+                </Form.Group>
+                
                 <Form.Group controlId='email' className='my-3'>
-                    <Form.Label>Email Address</Form.Label>
+                    <Form.Label>Email address</Form.Label>
                     <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
                 </Form.Group>
 
@@ -51,8 +63,13 @@ const LoginScreen = () => {
                     <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
 
+                <Form.Group controlId='confirmPassword' className='my-3'>
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control type='password' placeholder='Re-enter password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+
                 <Button type='submit' variant='primary' className='mt-2' disabled={ isLoading }>
-                    Log In
+                    Register
                 </Button>
 
                 { isLoading && <Loader /> }
@@ -60,12 +77,12 @@ const LoginScreen = () => {
 
             <Row className='py-3'>
                 <Col>
-                    New customer?{' '}
-                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                    Already have an account?{' '}
+                    <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Log In</Link>
                 </Col>
             </Row>
         </FormContainer>
     )
 }
 
-export default LoginScreen
+export default RegisterScreen
