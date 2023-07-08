57532710c34852f36abca4c19b2dbb765d7a01a6
Philip Rey Raquinel <devphil.work@gmail.com>
2023-07-08 20:22:42 +0800

    Add Route Component & Order List Screen for Admin

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -51,7 +51,8 @@ const Header = () => {
                                     }
                                 </Nav.Link>
                             </LinkContainer>
-                            { userInfo ? (
+
+                            {userInfo ? (
                                 <NavDropdown title={userInfo.name} id='username'>
                                     <LinkContainer to='/profile'>
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
@@ -66,8 +67,21 @@ const Header = () => {
                                         <FaUser /> Log In
                                     </Nav.Link>
                                 </LinkContainer>
-                            ) }
+                            )}
                             
+                            {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title='Admin' id='adminmenu'>
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
