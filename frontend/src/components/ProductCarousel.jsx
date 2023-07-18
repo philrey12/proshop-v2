bfb6b5a375b1ce124ea2cad8a5e4add678a507ab
Philip Rey Raquinel <devphil.work@gmail.com>
2023-07-19 00:21:25 +0800

    Center Carousel Images

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -17,7 +17,7 @@ const ProductCarousel = () => {
             {products.map(product => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} fluid />
+                        <Image src={product.image} alt={product.name} className='d-block mx-auto' fluid />
                         <Carousel.Caption className='carousel-caption'>
                             <h4>{product.name} (${product.price})</h4>
                         </Carousel.Caption>
