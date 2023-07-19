ae8cca0cb68a0f07172c85c1ae2e617d69b7fb86
Philip Rey Raquinel <devphil.work@gmail.com>
2023-07-20 01:47:26 +0800

    Add Build Script for Deployment Preparation

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ app.use(express.urlencoded({ extended: true }))
 // Cookie parser middleware
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -36,6 +32,18 @@ app.use('/api/upload', uploadRoutes)
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    // Any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
